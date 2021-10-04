45b92835c57c86760689de8b63e845840a84578e
Michael Levin <mglev1n@gmail.com>
2021-10-04 09:31:07 -0400

    Initial commit

---
+++ R/gg_regional_assoc.R
@@ -0,0 +1,145 @@
+# Function to plot regional association with LD
+#' gg_regional_assoc
+#'
+#' Returns a ggplot object containing a regional association plot (-log10(p-value) as a function of chromosomal position, with variants colored by linkage disequilibrium to reference variant).
+#' This function allows the user to integrate genome wide association study (GWAS) summary statistics for a locus of interest with linkage disequilibrium information (obtained using the LocusZoom API <https://portaldev.sph.umich.edu/>) for that locus to create a regional association plot.
+#'
+#' @param df Dataframe containing columns with rsid, chromosome, position, reference/effect allele, alternate/non-effect allele, and p-value for all variants within the range of interest
+#' @param lead_snps A character vector containing the lead variant of interest
+#' @param rsid Rsid column
+#' @param chromosome Chromosome column
+#' @param position Position column
+#' @param ref Reference/effect allele column
+#' @param alt Alternate/non-effect allele column
+#' @param p_value P-value column
+#' @param plot_pvalue_threshold Threshold for plotting p-value on regional association plot (default = 0.1) - reducing the number of points decreases file size and improves performance
+#' @param plot_distance Integer corresponding to the size of the locus that should be plotted
+#' @param genome_build Character - one of "GRCh37" or "GRCh38"
+#' @param population Character - one of "ALL", "AFR", "AMR", "EAS", "EUR", "SAS" referring to the reference population of interest for obtaining linkage disequilibrium information
+#' @param plot_title A character string corresponding to plot title (default = NULL)
+#' @param plot_subtitle A character string corresponding to plot subtitle (default = NULL)
+#' @param path Character string (default = NULL) - if a path is supplied a .pdf of the plot will be saved
+#'
+#' @return A ggplot object
+#' @export
+#'
+#' @examples
+gg_regional_assoc <- function(df, lead_snps, rsid = rsid, chromosome = chromosome, position = position, ref = ref, alt = alt, p_value = p_value, plot_pvalue_threshold = 0.1, plot_distance = 500000, genome_build = "GRCh37", population = "ALL", plot_title = NULL, plot_subtitle = NULL, path = NULL) {
+  df <- df %>%
+    select(rsid = {{ rsid }}, chromosome = {{ chromosome }}, position = {{ position }}, ref = {{ ref }}, alt = {{ alt }}, p_value = {{ p_value }}) %>%
+    mutate_if(is.factor, as.character) %>%
+    mutate(ref = stringr::str_to_upper(ref), alt = stringr::str_to_upper(alt))
+
+  indep_snps <- df %>%
+    select(lead_rsid = rsid, lead_chromosome = chromosome, lead_position = position, lead_ref = ref, lead_alt = alt) %>%
+    filter(lead_rsid %in% lead_snps)
+
+  # return(indep_snps)
+
+  locus_snps <- df %>%
+    filter(rsid %in% indep_snps$lead_rsid) %>%
+    select(chromosome, position, lead_rsid = rsid) %>%
+    pmap_dfr(function(chromosome_filter = first, position_filter = second, lead_rsid = third) {
+      df %>%
+        filter(chromosome == chromosome_filter & between(position, position_filter - plot_distance / 2, position_filter + plot_distance / 2)) %>%
+        dplyr::mutate(lead_rsid = lead_rsid) %>%
+        left_join(indep_snps)
+    })
+
+  # return(locus_snps)
+
+  # Extract LD and format colors
+  # consider adding error handling if we can't extract LD - eg. just plot without colors
+  possibly_ld_extract_locuszoom <- possibly(ld_extract_locuszoom_api, otherwise = NULL)
+
+  ld_extracted <- possibly_ld_extract_locuszoom(chrom = indep_snps$lead_chromosome, pos = indep_snps$lead_position, ref = indep_snps$lead_ref, alt = indep_snps$lead_alt, start = min(locus_snps$position), stop = max(locus_snps$position), build = genome_build, population = population)
+
+  if (!is.null(ld_extracted)) {
+    locus_snps_ld <- ld_extracted %>%
+      select(chromosome = chromosome2, position = position2, variant2, correlation) %>%
+      mutate(chromosome = as.numeric(chromosome), position = as.numeric(position)) %>%
+      separate(variant2, into = c("chr_pos", "ref_alt"), sep = "_") %>%
+      separate(ref_alt, into = c("ref", "alt"), sep = "/") %>%
+      right_join(locus_snps, by = c("chromosome" = "chromosome", "position" = "position")) %>%
+      filter((ref.x == ref.y & alt.x == alt.y) | (ref.x == alt.y & alt.x == ref.y)) %>%
+      select(-ends_with(".y"), -chr_pos) %>%
+      rename_with(~ str_replace(.x, ".x", ""), .cols = ends_with(".x")) %>%
+      mutate(color_code = as.character(cut(correlation, breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1), labels = c("blue4", "skyblue", "darkgreen", "orange", "red"), include.lowest = TRUE))) %>%
+      mutate(legend_label = as.character(cut(correlation, breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1), labels = c("0 - 0.2", "0.2 - 0.4", "0.4 - 0.6", "0.6 - 0.8", "0.8 - 1"), include.lowest = TRUE))) %>%
+      mutate(lead = rsid == lead_rsid) %>%
+      mutate(label = case_when(
+        rsid == lead_rsid ~ lead_rsid,
+        TRUE ~ NA_character_
+      )) %>%
+      mutate(color_code = case_when(
+        rsid == lead_rsid ~ "purple",
+        TRUE ~ color_code
+      )) %>%
+      mutate(color_code = fct_relevel(color_code, "purple", "red", "orange", "darkgreen", "skyblue", "blue4")) %>%
+      mutate(legend_label = case_when(
+        rsid == lead_rsid ~ "Reference Variant",
+        TRUE ~ legend_label
+      )) %>%
+      mutate(legend_label = fct_relevel(legend_label, "Reference Variant", "0.8 - 1", "0.6 - 0.8", "0.4 - 0.6", "0.2 - 0.4", "0 - 0.2"))
+  } else {
+    locus_snps_ld <- locus_snps %>%
+      mutate(correlation = NA_integer_) %>%
+      mutate(lead = rsid == lead_rsid) %>%
+      mutate(label = case_when(
+        rsid == lead_rsid ~ lead_rsid,
+        TRUE ~ NA_character_
+      )) %>%
+      mutate(color_code = case_when(
+        rsid == lead_rsid ~ "purple",
+        TRUE ~ color_code
+      )) %>%
+      mutate(color_code = fct_relevel(color_code, "purple", NA_character_)) %>%
+      mutate(legend_label = case_when(
+        rsid == lead_rsid ~ "Reference Variant",
+        TRUE ~ legend_label
+      )) %>%
+      mutate(legend_label = fct_relevel(legend_label, "Reference Variant", NA_character_))
+  }
+
+
+  # return(locus_snps_ld)
+
+  # Make Plot
+  regional_assoc_plot <- locus_snps_ld %>%
+    filter(p_value < plot_pvalue_threshold) %>%
+    # improve overplotting
+    arrange(desc(color_code)) %>%
+    ggplot(aes(position / plot_distance, -log10(p_value), fill = factor(color_code), size = lead, alpha = lead, shape = lead)) +
+    geom_point() +
+    ggrepel::geom_text_repel(aes(label = label), size = 4, color = "black", fontface = "bold", min.segment.length = 0, box.padding = 1, alpha = 1, nudge_x = -0.025 * max(locus_snps_ld$position) / plot_distance) +
+    geom_hline(yintercept = -log10(5e-8), linetype = "dashed") +
+    scale_fill_identity(parse(text = "r^2"), guide = "legend", labels = levels(locus_snps_ld$legend_label), na.translate = FALSE) +
+    scale_size_manual(values = c(3, 8), guide = "none") +
+    scale_shape_manual(values = c(21, 23), guide = "none") +
+    scale_alpha_manual(values = c(0.8, 1), guide = "none") +
+    scale_x_continuous(n.breaks = 3) +
+    guides(fill = guide_legend(override.aes = list(shape = 22, size = 8))) +
+    # facet_grid(trait ~ lead_rsid, scales = "free") +
+    labs(
+      # title = plot_title,
+      # subtitle = plot_subtitle,
+      x = glue::glue("Position on Chromosome {unique(indep_snps$lead_chromosome)} (Mb)"),
+      y = bquote(-log[10]("P-value"))
+    ) +
+    theme_light(base_size = 16) +
+    theme(
+      plot.title = element_text(face = "bold"),
+      legend.title.align = 0.5,
+      legend.key = element_rect(size = 6, fill = "white", colour = NA),
+      strip.background = element_rect(fill = "grey90"),
+      strip.text = element_text(color = "black", face = "bold"),
+      strip.text.x = element_blank()
+    )
+
+  if (is.null(path)) {
+    return(regional_assoc_plot)
+  } else {
+    ggsave(regional_assoc_plot, filename = paste0(path, unique(indep_snps$lead_rsid), ".pdf"), units = "in", height = 8.5, width = 11, device = "pdf")
+    return(regional_assoc_plot)
+  }
+}
---
+++ R/ld_extract_locuszoom.R
@@ -0,0 +1,86 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
+
+# Function to extract LD
+#' ld_extract_locuszoom
+#'
+#' This function allows the user to extract linkage disequilibrium statistics between a variant of interest and surrounding variants within a contiguous genomic region. This function uses the University of Michigan LocusZoom API <https://portaldev.sph.umich.edu/> to obtain LD information, and allows the user to specify genome-build and ancestry of interest.
+#'
+#' @param chrom Integer - chromosome of reference variant
+#' @param pos Integer - position of reference variant
+#' @param ref Character - reference allele (or effect allele) for reference variant
+#' @param alt Character - alternate allele (or non-effect allele) for reference variant
+#' @param start Integer - starting position of range of interest
+#' @param stop Integer - ending position of range of interest
+#' @param build Character - one of "GRCh37" or "GRCh38"
+#' @param population Character - one of "ALL", "AFR", "AMR", "EAS", "EUR", "SAS"
+#' @param metric Character - one of "r", "rsquare", or "cov", referring to the correlation statistic of interest
+#'
+#' @return A tibble containing each variant within the supplied range surrounding the variant of interest, with the requested linkage disequilibrium information with respect to the variant of interest
+#' @export
+#'
+#' @examples
+ld_extract_locuszoom <- function(chrom, pos, ref, alt, start, stop, build = "GRCh37", population = "ALL", metric = "rsquare") {
+
+  # Message
+  cli::cli_alert_info("Extracting LD for {chrom}:{pos}_{str_to_upper(ref)}/{str_to_upper(alt)} for the region {chrom}:{start}-{stop}")
+  # Format and run initial query
+  .json_res <- httr::GET(
+    url = glue::glue("https://portaldev.sph.umich.edu/ld/genome_builds/{build}/references/1000G/populations/{population}/variants"),
+    query = list(
+      correlation = metric,
+      variant = glue::glue("{chrom}:{pos}_{str_to_upper(ref)}/{str_to_upper(alt)}"),
+      chrom = chrom,
+      start = start,
+      stop = stop
+    )
+  )
+
+  # Parse query
+  .json_res_parsed <- .json_res %>% httr::content(as = "parsed", type = "application/json")
+
+  # Convert to tibble
+  .json_res_parsed_df <- .json_res_parsed$data %>%
+    as_tibble() %>%
+    unnest(cols = everything())
+
+  # If tibble is empty, reverse ref/alt
+  if (dim(.json_res_parsed_df)[1] == 0) {
+    # Message
+    cli::cli_alert_info("Initial query failed - trying again with flipped Ref/Alt")
+    # Format and run initial query
+    .json_res <- httr::GET(
+      url = glue::glue("https://portaldev.sph.umich.edu/ld/genome_builds/{build}/references/1000G/populations/{population}/variants"),
+      query = list(
+        correlation = metric,
+        variant = glue::glue("{chrom}:{pos}_{str_to_upper(alt)}/{str_to_upper(ref)}"),
+        chrom = chrom,
+        start = start,
+        stop = stop
+      )
+    )
+
+    # Parse query
+    .json_res_parsed <- .json_res %>% httr::content(as = "parsed", type = "application/json")
+
+    # Convert to tibble
+    .json_res_parsed_df <- suppressMessages(.json_res_parsed$data %>% as_tibble() %>% unnest(cols = everything()))
+  }
+
+  return(suppressMessages(.json_res_parsed_df %>% readr::type_convert()))
+}
+
