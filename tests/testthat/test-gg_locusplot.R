44c0121486c4242a281959de7e3befa0471fd111
Michael Levin <mglev1n@gmail.com>
2024-07-19 23:30:37 -0400

    update tests

--- tests/testthat/test-gg_locusplot.R
+++ tests/testthat/test-gg_locusplot.R
@@ -6,7 +6,7 @@ test_that("Incorrect function argument type returns error", {
 })
 
 test_that("Default gg_locusplot returns a ggplot object", {
-  locusplot_res <- gg_locusplot(df = fto_locus_df %>% mutate(trait = "BMI"), lead_snp = "rs62033413", rsid = rsid, chrom = chromosome, pos = position, ref = effect_allele, alt = other_allele, p_value = p_value, plot_genes = FALSE, plot_title = NULL, plot_subtitle = NULL, plot_distance = 1e6, path = NULL, trait = trait)
+  locusplot_res <- gg_locusplot(df = fto_locus_df %>% mutate(trait = "BMI"), lead_snp = "rs62033413", rsid = rsid, chrom = chromosome, pos = position, ref = effect_allele, alt = other_allele, p_value = p_value, plot_genes = FALSE, plot_recombination = TRUE, plot_title = NULL, plot_subtitle = NULL, plot_distance = 1e6, path = NULL, trait = trait)
   expect_s3_class(locusplot_res, "ggplot")
   # ggplot object returnd when lead_snp = NULL
   locusplot_res <- gg_locusplot(df = fto_locus_df, lead_snp = NULL, rsid = rsid, chrom = chromosome, pos = position, ref = effect_allele, alt = other_allele, p_value = p_value, plot_genes = FALSE, plot_title = NULL, plot_subtitle = NULL, plot_distance = 1e6, path = NULL)
